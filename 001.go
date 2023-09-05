b00fb19c0865bea25b213f6f605f617e1edad107
1s22s1 <1s22s1@example.com>
2023-09-05 18:41:35 +0900

    Solve 001 for Go language

---
+++ 001.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		fmt.Println(strings.ReplaceAll(scanner.Text(), " ", ""))
+	}
+}
--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 # code_golf_202309
+## How to Run
+### Go
+
+```console
+go run 001.go
+```
+
+### Ruby
+
+```console
+ruby 001.ruby
+```
+
 ## Count byte
 
 This is example for counting bytes.
